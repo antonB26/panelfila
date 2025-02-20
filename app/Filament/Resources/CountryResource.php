5e1fd0673e5d2cccf6166cb6b5dfbbfeeca48483
Antonio Balam <antonio.poblano@eleconomista.mx>
2025-02-20 17:37:31 -0600

    feat: System management de cada seccion

--- app/Filament/Resources/CountryResource.php
+++ app/Filament/Resources/CountryResource.php
@@ -19,6 +19,14 @@ class CountryResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-flag';
 
+    protected static ?string $navigationLabel = 'Country';
+
+    protected static ?string $modelLabel = 'Employees Country';
+
+    protected static ?string $navigationGroup = 'System Management';
+
+    protected static ?int $navigationSort = 1;
+
     public static function form(Form $form): Form
     {
         return $form
